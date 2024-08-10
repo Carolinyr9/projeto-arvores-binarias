de3833c2c13e1395f7d9978992c832c7b44bb0ce
Gabriel Barboza <65047738+gabrieIbarboza@users.noreply.github.com>
2024-08-09 22:00:13 -0300

    ✨ alocacao de massas nas arvores

--- main.c
+++ main.c
@@ -2,30 +2,14 @@
 #include <stdlib.h>
 #include "projeto-arvores.h"
 
+void alocarMassa(int dadosOrdenados, int numElementos, double *tempoAVL, double *tempoLLRB);
+
 int main() {
     char *fileName = "testeDados.csv";
     FILE *file = openFile(fileName);
-    arvAVL *arvoreAVL = cria_arvAVL();
-    arvoreLLRB *arvLLRB = cria_arvoreLLRB();
-
-    int check;
 
     int numEmployees = 13;
     Employee *employees = readFile(file, &numEmployees);
-
-    for(int i = 0; i < numEmployees; i++) {
-        check = insere_arvAVL(arvoreAVL, &employees[i]);
-        confirmaInsercaoArvoreAVL(check);
-    }
-
-    emOrdem_arvAVL(arvoreAVL);
-
-    for(int i = 0; i < numEmployees; i++) {
-        check = insere_arvoreLLRB(arvLLRB, &employees[i]);
-        confirmaInsercaoArvoreLLRB(check);
-    }
-
-    emOrdem_arvoreLLRB(arvLLRB);
 
     // Testando
     // printf("Desordenado\n");
@@ -40,7 +24,7 @@ int main() {
     fileName = "dadosOrdenados.csv";
     saveToFile(sortedEmployees, numEmployees, fileName);
 
-    for (int i = 0; i < numEmployees; i++) {
+    for(int i = 0; i < numEmployees; i++) {
         free(employees[i].name);
         free(employees[i].company);
         free(employees[i].department);
@@ -49,11 +33,72 @@ int main() {
     free(employees);
     free(sortedEmployees);
     closeFile(file);
-    liberar_arvAVL(arvoreAVL);
-    liberar_arvoreLLRB(arvLLRB);
+
+    //
+    // ALEX: MENU
+    //
+    double tempoAVL;
+    double tempoLLRB;
+    // Ordenados
+    alocarMassa(1, numEmployees, &tempoAVL, &tempoLLRB);
+    // Desordenados
+    alocarMassa(0, numEmployees, &tempoAVL, &tempoLLRB);
+
 
     printf("\n\n");
     system("pause");
     return 0;
+}
+
+void alocarMassa(int dadosOrdenados, int numElementos, double *tempoAVL, double *tempoLLRB)
+{
+    int check;
+    arvAVL *arvoreAVL = cria_arvAVL();
+    arvoreLLRB *arvLLRB = cria_arvoreLLRB();
+    char *fileName;
+
+    if(dadosOrdenados == 1)
+    {
+        fileName = "dadosOrdenados.csv";
+    }
+    else
+    {
+        fileName = "testeDados.csv";
+    }
+
+    //CAROL: Começar a contabilizar o tempo para AVL
+
+    FILE *file = openFile(fileName);
+
+    Employee *employees = readFile(file, &numElementos);
+
+    for(int i = 0; i < numElementos; i++) {
+        check = insere_arvAVL(arvoreAVL, &employees[i]);
+        confirmaInsercaoArvoreAVL(check);
+    }
+
+    emOrdem_arvAVL(arvoreAVL);
+
+    closeFile(file);
+
+    // CAROL Parar de contabilizar o tempo AVL
+
+    // CAROL Começar contabilizar o tempo LLRB
+
+    file = openFile(fileName);
+
+    for(int i = 0; i < numElementos; i++) {
+        check = insere_arvoreLLRB(arvLLRB, &employees[i]);
+        confirmaInsercaoArvoreLLRB(check);
+    }
+
+    emOrdem_arvoreLLRB(arvLLRB);
+
+    closeFile(file);
+
+    // CAROL Parar de contabilizar o tempo LLRB
+
+    liberar_arvAVL(arvoreAVL);
+    liberar_arvoreLLRB(arvLLRB);
 }
 
